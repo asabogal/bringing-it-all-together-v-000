5f72529150c932ba0e2170275b961f47167a1be0
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-13 23:27:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,16 @@ class Dog
     dog
   end
 
-  def self.find_by_id(row)
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+      LIMIT 1
+      SQL
+      DB[:conn].execute(sql, id).map do |row|
+        self.new_from_db(row)
+      end.first
+    
   end
 
 
