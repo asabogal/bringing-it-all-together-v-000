52a97363e25c9647461b7e1cf3551b9f943611ab
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-14 00:19:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,14 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_attrs = dog[0]
+      dog = self.new(dog_attrs[0], dog_attrs[1], dog_attrs[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
 
 end
